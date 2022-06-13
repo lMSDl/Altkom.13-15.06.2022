10676a3de7be220a859ea3efa27574b3fab7f59a
Paweł Łęczycki <pawel@msd.info.pl>
2022-06-13 12:10:39 +0200

    Interface Segregation Principle

---
+++ WPC/SOLID/IV_I.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.SOLID
+{
+    interface  IPdfFormatter
+    {
+        object ToPdf();
+    }
+
+    interface IExcelFormatter
+    {
+        object ToExcel();
+    }
+
+    class Report : IPdfFormatter, IExcelFormatter
+    {
+        public object ToExcel()
+        {
+            Console.WriteLine("Excel generated");
+            return new object();
+        }
+
+        public object ToPdf()
+        {
+            var obj = ToExcel();
+            Console.WriteLine("Pdf generated");
+            return new object();
+        }
+    }
+
+    class Poem : IPdfFormatter
+    {
+        public object ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+            return new object();
+        }
+    }
+}

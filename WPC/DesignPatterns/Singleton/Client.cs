72cff72003ec2fae0c5a1ecba7164ad93a85067b
Paweł Łęczycki <pawel@msd.info.pl>
2022-06-13 16:02:55 +0200

    Singleton (1)

---
+++ WPC/DesignPatterns/Singleton/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Singleton
+{
+    public class Client
+    {
+        public static void Execute()
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                _ = Task.Run(() => Context.GetInstance().GetSettings("1"));
+            }
+
+
+            var value1 = Context.GetInstance().GetSettings("1");
+            Console.WriteLine($"1: {value1}");
+
+            var value2 = Context.GetInstance().GetSettings("2");
+            Console.WriteLine($"2: {value2}");
+
+            Context.GetInstance().SetSettings("3", "x");
+
+            var value3 = Context.GetInstance().GetSettings("3");
+            Console.WriteLine($"3: {value3}");
+
+        }
+    }
+}
---
+++ WPC/DesignPatterns/Singleton/Context.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Singleton
+{
+    public class Context
+    {
+
+        private readonly Dictionary<string, string> _settings = new Dictionary<string, string>();
+
+        private Context()
+        {
+            Console.WriteLine("Inicjalizacja ustawień");
+            _settings["1"] = "a";
+            _settings["2"] = "b";
+            _settings["3"] = "c";
+        }
+
+        public string GetSettings(string key)
+        {
+            return _settings[key];
+        }
+
+        public void SetSettings(string key, string value)
+        {
+            _settings[key] = value;
+        }
+
+        private static Context _instance;
+
+        //public static Context GetInstance()
+        //{
+        //    if (_instance == null)
+        //        _instance = new Context();
+        //    return _instance;
+        //}
+
+        private static object _locker = new object();
+        //public static Context GetInstance()
+        //{
+        //    lock (_locker)
+        //    {
+        //        if (_instance == null)
+        //            _instance = new Context();
+        //    }
+        //    return _instance;
+        //}
+        public static Context GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_locker)
+                {
+                    if (_instance == null)
+                        _instance = new Context();
+                }
+            }
+            return _instance;
+        }
+    }
+}

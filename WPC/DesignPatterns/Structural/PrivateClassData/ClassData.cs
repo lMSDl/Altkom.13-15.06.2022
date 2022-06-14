41f89495a9d78041413b68b78027c37fc06cfd4d
Paweł Łęczycki <pawel@msd.info.pl>
2022-06-14 14:45:13 +0200

    PrivateClassData

---
+++ WPC/DesignPatterns/Structural/PrivateClassData/ClassData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Structural.PrivateClassData
+{
+    public class ClassData
+    {
+        //private int _intValue;
+        //private string _stringValue;
+
+        private PrivateClassData _privateClassData;
+
+        public ClassData(int intValue, string stringValue)
+        {
+            _privateClassData = new PrivateClassData(intValue, stringValue);
+        }
+
+
+
+        public void DoSth()
+        {
+            //_intValue = 2;
+        }
+    }
+}
---
+++ WPC/DesignPatterns/Structural/PrivateClassData/ClassData2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Structural.PrivateClassData
+{
+    public class ClassData2 : PrivateClassData
+    {
+        public ClassData2(int intValue, string stringValue) : base(intValue, stringValue)
+        {
+        }
+
+        public void DoSth()
+        {
+
+        }
+    }
+}

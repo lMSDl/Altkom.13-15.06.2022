03e85be267748b13399525c6f76d46ba52ef12e7
Paweł Łęczycki <pawel@msd.info.pl>
2022-06-15 11:49:13 +0200

    Memento

--- WPC/Program.cs
+++ WPC/Program.cs
@@ -7,7 +7,7 @@ namespace WPC
     {
         static void Main(string[] args)
         {
-            WPC.DesignPatterns.Behavioral.Mediator.Client.Execute();
+            WPC.DesignPatterns.Behavioral.Memento.Client.Execute();
         }
 
         private static void LSP()

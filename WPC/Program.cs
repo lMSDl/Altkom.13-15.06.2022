e073ad26009a2fc0bdd9745a8563f3766b8d3258
Paweł Łęczycki <pawel@msd.info.pl>
2022-06-15 09:44:02 +0200

    Command

--- WPC/Program.cs
+++ WPC/Program.cs
@@ -7,7 +7,7 @@ namespace WPC
     {
         static void Main(string[] args)
         {
-            WPC.DesignPatterns.Behavioral.ChainOfResponsibility.II.Client.Execute();
+            WPC.DesignPatterns.Behavioral.Command.Client.Execute();
         }
 
         private static void LSP()
